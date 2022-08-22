eca42ac44c6a00d542d30fbf49c18bc93858b0ad
Sami Koivisto <sami.koivisto.dev@gmail.com>
2022-08-23 00:22:47 +0300

    Refactor console messages out from main function

--- HashGrinder/Program.cs
+++ HashGrinder/Program.cs
@@ -26,9 +26,7 @@ for (int round = 1; round <= roundCount; round++)
 {
     prompt = hasher.Hash(seed);
 
-    Console .WriteLine($"--- ROUND {round} ---");
-    Console.WriteLine($"Seed: {Convert.ToHexString(seed)}");
-    Console.WriteLine($"Target: {Convert.ToHexString(prompt)}");
+    ShowInfo.RoundStart(round, seed, prompt);
     var cycleTimer = Stopwatch.StartNew();
 
     const byte maxLength = byte.MaxValue;
@@ -45,21 +43,7 @@ for (int round = 1; round <= roundCount; round++)
     }
 
     cycleTimer.Stop();
-    Console.WriteLine();
-    Console.WriteLine($"--- FINISHED ---");
-    Console.WriteLine($"Target: {Convert.ToHexString(prompt)}");
-
-    if (firstMatch == null)
-    {
-        Console.WriteLine($"First match: Not found.");
-        Console.WriteLine($"Time: {cycleTimer.ElapsedMilliseconds} ms");
-        return;
-    }
-
-    Console.WriteLine($"First match: {Convert.ToHexString(firstMatch)}");
-
-    Console.WriteLine($"Time: {cycleTimer.ElapsedMilliseconds} ms");
-    Console.ReadLine();
+    ShowInfo.RoundResults(seed, prompt, firstMatch, cycleTimer.ElapsedMilliseconds);
 
     // Generate prompt for the next round
     seed = seedGenerator.Merge(prompt, firstMatch);
---
+++ HashGrinder/ShowInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashGrinder
+{
+    internal static class ShowInfo
+    {
+        public static void RoundStart(int round, byte[] seed, byte[] prompt)
+        {
+            Console.WriteLine($"--- ROUND {round} ---");
+            Console.WriteLine($"Seed: {Convert.ToHexString(seed)}");
+            Console.WriteLine($"Target: {Convert.ToHexString(prompt)}");
+        }
+
+        public static void RoundResults(byte[] seed, byte[] prompt, byte[]? firstMatch, long elapsedMilliseconds)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"--- FINISHED ---");
+            Console.WriteLine($"Target: {Convert.ToHexString(prompt)}");
+
+            if (firstMatch == null)
+            {
+                Console.WriteLine($"First match: Not found.");
+                Console.WriteLine($"Time: {elapsedMilliseconds} ms");
+                return;
+            }
+
+            Console.WriteLine($"First match: {Convert.ToHexString(firstMatch)}");
+
+            Console.WriteLine($"Time: {elapsedMilliseconds} ms");
+            Console.ReadLine();
+        }
+    }
+}
