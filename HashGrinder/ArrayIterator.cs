e3fcdd64c0f4d30d309edc4c1ba39dddcbf93c54
Sami Koivisto <sami.koivisto.dev@gmail.com>
2022-08-22 22:53:11 +0300

    Refactored hash iteration into separate class

---
+++ HashGrinder/ArrayIterator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashGrinder
+{
+    internal class ArrayIterator
+    {
+        private readonly IHash _hasher;
+
+        public ArrayIterator(IHash hasher)
+        {
+            _hasher = hasher;
+        }
+
+        public byte[]? Iterate(int length, byte[] reference)
+        {
+            var bytes = new byte[length];
+            UInt64 maxChanges = Convert.ToUInt64(Math.Pow(byte.MaxValue, bytes.Length));
+
+            Console.WriteLine();
+            Console.WriteLine($"Iteration {length}, {maxChanges} individual values");
+
+            // Assign values
+            for (UInt64 j = 0; j <= maxChanges; j++)
+            {
+                // Over the cell max value
+                if (bytes[0] == byte.MaxValue)
+                {
+                    // Iterate through byte array to resolve the increment
+                    for (int h = 1; h < bytes.Length; h++)
+                    {
+                        if (bytes[h] == byte.MaxValue)
+                        {
+                            bytes[h] = 0;
+                            continue;
+                        }
+
+                        bytes[h]++;
+                    }
+
+                    bytes[0] = 0;
+                    continue;
+                }
+
+                var hash = _hasher.Hash(bytes);
+                var matchFound = hash.Equals(reference);
+
+                if (matchFound)
+                    return bytes;
+
+                bytes[0]++;
+            }
+
+            return null;
+        }
+
+    }
+}
--- HashGrinder/Program.cs
+++ HashGrinder/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 
 IHash hasher = new Hash_SHA256();
+ArrayIterator iterator = new(hasher);
 
 var testPrompt = "FF1151AFB20E9E4564838EF8075846449287597A4CDD7F598AFF8B36231792B1";
 
@@ -24,54 +25,19 @@ for (int i = 0; i < arrayLength; i++)
 //const byte maxLength = byte.MaxValue;
 const byte maxLength = 3;
 byte[] bytes = new byte[1];
-var hash = new byte[1];
-var matchFound = false;
 
 var timer = new Stopwatch();
 var totalTimer = Stopwatch.StartNew();
 
+byte[]? firstMatch = null;
+
 // Generate array with extending length
 for (int i = 1; i <= maxLength; i++)
 {
-    bytes = new byte[i];
-    UInt64 maxChanges = Convert.ToUInt64(Math.Pow(byte.MaxValue, i));
-
-    Console.WriteLine();
-    Console.WriteLine($"Iteration {i}, {maxChanges} individual values");
     timer.Restart();
+    firstMatch = iterator.Iterate(i, promptBytes);
 
-    // Assign values
-    for (UInt64 j = 0; j <= maxChanges; j++)
-    {
-        // Over the cell max value
-        if (bytes[0] == byte.MaxValue)
-        {
-            // Iterate through byte array to resolve the increment
-            for (int h = 1; h < i; h++)
-            {
-                if (bytes[h] == byte.MaxValue)
-                {
-                    bytes[h] = 0;
-                    continue;
-                }
-
-                bytes[h]++;
-            }
-
-            bytes[0] = 0;
-            continue;
-        }
-
-        hash = hasher.Hash(bytes);
-        matchFound = hash.Equals(promptBytes);
-
-        if (matchFound)
-            break;
-
-        bytes[0]++;
-    }
-
-    if (matchFound)
+    if (firstMatch != null)
         break;
 
     Console.WriteLine($"DONE ({timer.ElapsedMilliseconds} ms)");
@@ -83,8 +49,12 @@ timer.Stop();
 Console.Write($"Prompt: {Convert.ToHexString(promptBytes)}");
 Console.WriteLine();
 
-Console.Write($"FirstMatch: {Convert.ToHexString(hash)}");
+if (firstMatch != null)
+    Console.Write($"FirstMatch: {Convert.ToHexString(firstMatch)}");
+else
+    Console.Write($"No match found.");
 Console.WriteLine();
+
 
 Console.WriteLine($"FINISHED in {totalTimer.ElapsedMilliseconds} ms");
 Console.ReadLine();
